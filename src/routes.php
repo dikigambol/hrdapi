038d324597e0e2c46789fd8b8d34bc3f3fd43d25
Zain Bagus <ancientlord123@gmail.com>
2021-12-14 10:12:32 +0700

    Detail Izin Koordinator + Rektor ID

--- src/routes.php
+++ src/routes.php
@@ -2444,6 +2444,7 @@ return function (App $app) {
 		$id = trim($plaintext_dec);
 
 		$idKoor = $_GET['id_koor'];
+		$idKoorDecrypt = decryptKoor($idKoor);
 
 		$sql = "SELECT * FROM `user_entity` WHERE `id` = '$id' ";
 		$stmt = $this->db->prepare($sql);
@@ -2451,7 +2452,11 @@ return function (App $app) {
 		$res = array();
 
 		while ($result = $stmt->fetch()) {
-			$getIzin = "SELECT * FROM `izin_hrd` WHERE `id_user` = '$id' AND `acc1` = '' OR `acc2` = '' ORDER BY tgl_dibuat DESC";
+			$getIzin = "SELECT * FROM `izin_hrd` WHERE `id_user` = '$id' AND `acc1` = '$idKoorDecrypt' OR `acc2` = '$idKoorDecrypt' ORDER BY tgl_dibuat DESC";
+			$getRektorID = "SELECT rektor_id FROM struktural";
+			$prepsListRektorID = $this->db->prepare($getRektorID);
+			$prepsListRektorID->execute();
+			$listRektorID = $prepsListRektorID->fetchAll();
 			$prepsListIzin = $this->db->prepare($getIzin);
 			$prepsListIzin->execute();
 			$listIzin = $prepsListIzin->fetchAll();
@@ -2462,6 +2467,7 @@ return function (App $app) {
 			$h['jabatan'] = $result['jabatan'];
 			$h['koordinator'] = $result['koordinator'];
 			$h['list_izin'] = $listIzin;
+			$h['jabatan_khusus'] = $listRektorID;
 
 			array_push($res, $h);
 		}
@@ -2469,6 +2475,33 @@ return function (App $app) {
 		return $response->withJson($res, 200);
 	});
 	// end detail izin koordinator
+
+	//global function decrypt
+	function decryptKoor($param1) {
+		$ciphertext_base64 = $param1;
+		$key = "ExpertuptsiINSTITUT_ASIA";
+
+		$method = "AES-128-CTR";
+		$option = 0;
+		$iv2 = "1251632135716362";
+
+		$key_size =  strlen($key);
+
+		$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+		$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+
+		$ciphertext_dec = base64_decode(base64_decode($ciphertext_base64));
+
+
+		$dataDecrypt=openssl_decrypt($ciphertext_dec, $method, $key, $option, $iv2);
+
+		$iv_dec = substr($dataDecrypt, 0, $iv_size);
+		$ciphertext_decas = substr($dataDecrypt, $iv_size);
+
+		$plaintext_dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $ciphertext_decas, MCRYPT_MODE_CBC, $iv_dec);
+
+		return $plaintext_dec;
+	}
 
 	// login astor hrd 
 	$app->get("/hrd/sign/id/{cari}", function (Request $request, Response $response) {
